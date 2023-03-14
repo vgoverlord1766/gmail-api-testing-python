915884a3c6ef89d9ad9239d8c077340f93009eea
vgoverlord1766 <jgomez@thinkbox.us>
2023-03-14 17:53:39 -0400

    a'

---
+++ better_main.py
@@ -0,0 +1,48 @@
+from __future__ import print_function
+import json
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/calendar.events.readonly',
+          'https://www.googleapis.com/auth/tasks']
+
+
+def is_user_authorized(user_lasid):
+    with open('token.json', 'r') as token_file:
+        return user_lasid in json.load(token_file)
+
+
+def get_user_credentials(user_lasid):
+    if is_user_authorized(user_lasid):
+        with open('token.json', 'r') as token_file:
+            user_credentials_json = json.load(token_file)[user_lasid]
+        user_credentials = Credentials.from_authorized_user_info(user_credentials_json)
+        return user_credentials
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        credentials = flow.run_local_server(port=0)
+        with open('token.json', 'r') as token_file:
+            data = json.loads(token_file.read())
+            users = data
+            print(users)
+        users[user_lasid] = json.loads(credentials.to_json())
+        with open('token.json', 'w') as token_file:
+            token_file.write(json.dumps(users))
+
+
+print("Enter your LASID:")
+user_input = input()
+
+print(get_user_credentials(user_input))
+creds = get_user_credentials(user_input)
+service = build('gmail', 'v1', credentials=creds)
+results = service.users().labels().list(userId='me').execute()
+labels = results.get('labels', [])
+
+if not labels:
+    print('No labels found.')
+print('Labels:')
+for label in labels:
+    print(label['name'])
